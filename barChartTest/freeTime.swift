0fc013445c6224635c11e5143efd29bd2254891a
adam <adamjeanlaurent@gmail.com>
2019-08-01 10:23:16 -0700

    first commit

---
+++ barChartTest/freeTime.swift
@@ -0,0 +1,14 @@
+//
+//  freeTime.swift
+//  barChartTest
+//
+//  Created by Adam Jean-Laurent on 8/1/19.
+//  Copyright © 2019 Adam Jean-Laurent. All rights reserved.
+//
+
+import Foundation
+
+struct FreeTimeCount {
+    var showNumber: String
+    var viewCount: Double
+}
